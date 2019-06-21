d9c98a65838cc250e5a9d64068f4ccb763e33ead
Mark McNally <mcnal1987@gmail.com>
2019-06-21 17:32:04 -0500

    for loop is appending list items to the DOM.Everything is working

--- main js/main.js
+++ main js/main.js
@@ -1,14 +1,18 @@
 $(document).ready(onReady);
 
 let employeeArray = [];
+let totalSalary = 0;
 
+//intializes to the click, and pushes the employee list to the array.
 function onReady() {
     console.log('jq');
 $('#addItemButton').on('click', addEmployee);
+
 }
 
 
 function addEmployee(){
+    event.preventDefault();
     let employee = {
         firstName: $('#firstNameIn').val(),
         lastName: $('#lastNameIn').val(),
@@ -16,8 +20,42 @@ function addEmployee(){
         jobTitle: $('#jobTitleIn').val(),
         annualSalary: $('#salaryIn').val()
     }
-    console.log('new employee', employee);
-    console.log('hello');
+    //console.log('new employee', employee);
+    //console.log('hello');
     employeeArray.push(employee)
+    showEmployeeList();
+    removeEmployee();
+    showTotalSalary(employee.annualSalary);
 }
+
+//show employee list to the DOM
+function showEmployeeList(){
+    let el = $('#element');
+    $('.removeAll').val('');
+    //console.log('showEmployeeList');
+    for (let i = 0; i < employeeArray.length; i++) {
+         el.append(`<tr>
+         <th>${employeeArray[i].firstName}</th>
+         <th>${employeeArray[i].lastName}</th>
+         <th>${employeeArray[i].idNumber}</th>
+         <th>${employeeArray[i].jobTitle}</th>
+         <th>${employeeArray[i].annualSalary}</th>
+         <th> 
+         </tr>`);
+    } //if(`${employeeArray[i].annualSalary}` > )
+ }
+ function removeEmployee(task){
+     
+     //console.log('remove employee');
+ }
+
+ function showTotalSalary(salary){
+   salary = Number(salary);
+    totalSalary += salary;
+ console.log('showTotalSalary', totalSalary);
+ $('#outputDiv').append(salary);
+ }
+ // another function that calculates the total and lists it on the DOM
+ //employee.annual salary.
+
 
