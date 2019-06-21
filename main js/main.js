689a447f07fda18831fbbd887db4aeae61462aea
Mark McNally <mcnal1987@gmail.com>
2019-06-21 15:29:54 -0500

    added employeearray and it logs to console.

--- main js/main.js
+++ main js/main.js
@@ -2,7 +2,22 @@ $(document).ready(onReady);
 
 let employeeArray = [];
 
-function onReady(){
+function onReady() {
+    console.log('jq');
+$('#addItemButton').on('click', addEmployee);
+}
+
+
+function addEmployee(){
+    let employee = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        idNumber: $('#idIn').val(),
+        jobTitle: $('#jobTitleIn').val(),
+        annualSalary: $('#salaryIn').val()
+    }
+    console.log('new employee', employee);
     console.log('hello');
+    employeeArray.push(employee)
 }
 
